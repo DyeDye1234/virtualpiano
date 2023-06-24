adfe9fdc1568a5b20e8be45d5031ff52ebfa3442
Theerapat Muangpoon <chansk131@gmail.com>
2023-06-24 14:50:18 +0700

    Fix music sheet image

--- src/Sheet.jsx
+++ src/Sheet.jsx
@@ -1,11 +1,11 @@
-import Sheet1 from './assets/Sheet1.PNG';
-import './Sheet.css';
+import Sheet1 from "./assets/amazing-grace.png"
+import "./Sheet.css"
 
 function SheetMusic() {
-    return (
-        <div className='sheet'>
-            <img src={Sheet1}></img>
-        </div>
-    )
+  return (
+    <div className="sheet">
+      <img src={Sheet1}></img>
+    </div>
+  )
 }
 export default SheetMusic

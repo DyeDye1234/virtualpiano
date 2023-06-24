b36c84fbc3328bcbb3e76631d473244d8660100b
DyeDye1234 <72293878+DyeDye1234@users.noreply.github.com>
2023-06-24 14:43:29 +0700

    commit2

---
+++ src/Sheet.jsx
@@ -0,0 +1,11 @@
+import Sheet1 from './assets/Sheet1.PNG';
+import './Sheet.css';
+
+function SheetMusic() {
+    return (
+        <div className='sheet'>
+            <img src={Sheet1}></img>
+        </div>
+    )
+}
+export default SheetMusic

15ddd3233515b08fe247c77fae45915e706c4b2e
Theerapat Muangpoon <chansk131@gmail.com>
2023-07-08 17:25:35 +0700

    fix styling

--- src/Sheet.jsx
+++ src/Sheet.jsx
@@ -5,12 +5,12 @@ import React from "react"
 function SheetMusic() {
   const containerElement = React.useRef(null)
   return (
-    <div>
+    <>
       <div className="sheet" ref={containerElement}>
         <img src={Sheet1}></img>
       </div>
       <button onClick={() => { containerElement.current.scrollTop += 200 }}>DOWN</button>
-    </div>
+    </>
   )
 }
 export default SheetMusic

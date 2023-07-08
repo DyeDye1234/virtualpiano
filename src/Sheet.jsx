8bf223c66a441bb6a5cc406aaea06784d4204e56
DyeDye1234 <72293878+DyeDye1234@users.noreply.github.com>
2023-07-08 14:42:14 +0700

    firebase

--- src/Sheet.jsx
+++ src/Sheet.jsx
@@ -9,7 +9,7 @@ function SheetMusic() {
       <div className="sheet" ref={containerElement}>
         <img src={Sheet1}></img>
       </div>
-      <button onClick={() => { containerElement.current.scrollTop += 10 }}>DOWN</button>
+      <button onClick={() => { containerElement.current.scrollTop += 200 }}>DOWN</button>
     </div>
   )
 }

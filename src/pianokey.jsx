00618a0aec45d70db334403d3aa5cd85417b7547
Theerapat Muangpoon <chansk131@gmail.com>
2023-08-05 15:03:40 +0700

    fix useEffect dependency

--- src/pianokey.jsx
+++ src/pianokey.jsx
@@ -1,35 +1,47 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react"
 
-function PianoKey({ type, children, onPress }) {
-    const [pressed, setPressed] = useState(false)
-    useEffect(() => {
-        const onKeyDown = (event) => {
-            console.log(event.key)
-            if (event.key.toUpperCase() === children) {
-                setPressed(true)
-                onPress?.()
-            }
-        }
+function PianoKey({ keyType, children, onPress }) {
+  const [pressed, setPressed] = useState(false)
+  useEffect(() => {
+    const onKeyDown = (event) => {
+      console.log(event.key)
+      if (event.key.toUpperCase() === children) {
+        setPressed(true)
+        console.log("onPress")
+        onPress?.()
+      }
+    }
 
-        const onKeyUp = (event) => {
-            if (event.key.toUpperCase() === children) {
-                setPressed(false)
-            }
-        }
+    const onKeyUp = (event) => {
+      if (event.key.toUpperCase() === children) {
+        setPressed(false)
+      }
+    }
 
-        window.addEventListener("keydown", onKeyDown)
-        window.addEventListener("keyup", onKeyUp)
+    window.addEventListener("keydown", onKeyDown)
+    window.addEventListener("keyup", onKeyUp)
 
-        return () => {
-            window.removeEventListener("keydown", onKeyDown)
-            window.removeEventListener("keyup", onKeyUp)
-        }
+    return () => {
+      window.removeEventListener("keydown", onKeyDown)
+      window.removeEventListener("keyup", onKeyUp)
+    }
+  }, [children, onPress])
 
-    }, [])
-
-    const pressedStyle = pressed ? "pressed" : " "
-    const keyStyle = type === "black" ? "bkey" : "wkey"
-    return <button onMouseDown={function () { setPressed(true) }} onMouseUp={function () { setPressed(false) }} className={`key ${keyStyle} ${pressedStyle}`}>{children}</button>
+  const pressedStyle = pressed ? "pressed" : " "
+  const keyStyle = keyType === "black" ? "bkey" : "wkey"
+  return (
+    <button
+      onMouseDown={function () {
+        setPressed(true)
+      }}
+      onMouseUp={function () {
+        setPressed(false)
+      }}
+      className={`key ${keyStyle} ${pressedStyle}`}
+    >
+      {children}
+    </button>
+  )
 }
 
 export default PianoKey
--- src/virtualpiano.jsx
+++ src/virtualpiano.jsx
@@ -1,47 +1,50 @@
 import { useState, useRef, useEffect } from "react"
 import "./virtualpiano.css"
-import PianoKey from "./pianokey";
-import { SplendidGrandPiano } from "smplr";
+import PianoKey from "./pianokey"
+import { SplendidGrandPiano } from "smplr"
 
 function VirtualPiano() {
-    const [status, setStatus] = useState("start");
-    const [piano, setPiano] = useState(undefined);
+  const [status, setStatus] = useState("start")
+  const [piano, setPiano] = useState(undefined)
 
-    const loadPiano = () => {
-        if (piano) return;
-        setStatus("loading");
-        const newPiano = new SplendidGrandPiano(new AudioContext());
-        setPiano(newPiano);
-        newPiano.loaded().then(() => {
-            setStatus("ready");
-        });
-    };
-    return (<div>
-        <div>{status}</div>
-        <button onClick={() => loadPiano()}>Start</button>
-        <button onClick={() => piano?.start({ note: "C3" })}>Play</button>
-        <div className="container">
-            <PianoKey onPress={() => {
-                console.log("C3")
-                piano?.start({ note: "C3" })
-            }}>Q</PianoKey>
+  const loadPiano = () => {
+    if (piano) return
+    setStatus("loading")
+    const newPiano = new SplendidGrandPiano(new AudioContext())
+    setPiano(newPiano)
+    newPiano.loaded().then(() => {
+      setStatus("ready")
+    })
+  }
+  return (
+    <div>
+      <div>{status}</div>
+      <button onClick={() => loadPiano()}>Start</button>
+      <button onClick={() => piano?.start({ note: "C3" })}>Play</button>
+      <div className="container">
+        <PianoKey
+          onPress={() => {
+            console.log("C3")
+            piano?.start({ note: "C3" })
+          }}
+        >
+          Q
+        </PianoKey>
 
-            <PianoKey type="black">2</PianoKey>
-            <PianoKey>W</PianoKey>
-            <PianoKey type="black">3</PianoKey>
-            <PianoKey>E</PianoKey>
-            <PianoKey>R</PianoKey>
-            <PianoKey type="black">5</PianoKey>
-            <PianoKey>T</PianoKey>
-            <PianoKey type="black">6</PianoKey>
-            <PianoKey>Y</PianoKey>
-            <PianoKey type="black">7</PianoKey>
-            <PianoKey>U</PianoKey>
-        </div>
-    </div>)
-
-
-
+        <PianoKey keyType="black">2</PianoKey>
+        <PianoKey>W</PianoKey>
+        <PianoKey keyType="black">3</PianoKey>
+        <PianoKey>E</PianoKey>
+        <PianoKey>R</PianoKey>
+        <PianoKey keyType="black">5</PianoKey>
+        <PianoKey>T</PianoKey>
+        <PianoKey keyType="black">6</PianoKey>
+        <PianoKey>Y</PianoKey>
+        <PianoKey keyType="black">7</PianoKey>
+        <PianoKey>U</PianoKey>
+      </div>
+    </div>
+  )
 }
 
 export default VirtualPiano
